4d068878087e2889c8f0a9aa8eb4599f8a516edd
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2025-09-19 00:14:27 +0200

    funcoes simples

---
+++ pages/api/executar.js
@@ -0,0 +1,87 @@
+
+let acaoGlobal = null;
+let estadoGlobal = null;
+let lastActionGlobal = null;
+
+
+export default async function handler(req, res) {
+    try {
+        const { action } = req.query;
+        console.log(`Received ${req.method} request with action: ${action}`);
+
+        if (!action) {
+            return res.status(400).json({ error: 'Action is required' });
+        }
+
+        if (req.method === 'POST') {
+            const {acao,  func } = req.body;
+            switch (action) {
+
+                case 'direto':
+
+                      if (!lastActionGlobal){
+                        console.log('não há açoes')
+                      if (func === 'Nivel'){
+                          console.log('Executar func');
+                            try{
+                              acaoGlobal = "A11";
+                              console.log('Setando acaoglobal para ',{acaoGlobal});
+                              res.status(201).json(acaoGlobal);
+                              lastActionGlobal = 'executando';
+                            } catch (error) {
+                                res.status(500).json({ error: "Failed to create action" });
+                              } 
+                        }else if (func === 'Motor'){
+                          console.log('Executar func');
+                            try{
+                            acaoGlobal = "A21";
+                            console.log('Setando acaoglobal para',{acaoGlobal});
+                              res.status(201).json(acaoGlobal);
+                              lastActionGlobal = 'executando';
+                            } catch (error) {
+                                res.status(500).json({ error: "Failed to create action" });
+                              } 
+                        }else{
+                          console.log('Ação incorreta');
+                          res.status(201).json(acaoGlobal);
+                        };
+                      }else{
+                        console.log('executando ainda');
+                        res.status(201).json(acaoGlobal);
+                      }
+
+                      if(acao === "A60"){
+                        acaoGlobal = "A60";
+                        setTimeout(() => {
+                          acaoGlobal = null;
+                          console.log('Variável global estadoGlobal configurada para null após 2 minutos.');
+                          }, 20 * 1000); // 2 minutos em milissegundos
+                      }
+
+
+                        break;
+                default:
+                    res.status(400).json({ error: 'Invalid action for POST request.' });
+            }
+        } else if (req.method === 'GET') {
+            switch (action) {
+                case 'acao':
+                    console.log('Acao enviada ao get', acaoGlobal);
+                    res.status(200).json(acaoGlobal);
+                    break;
+                case 'estado':
+                    console.log('Estado enviado ao get', estadoGlobal);
+                    res.status(200).json(estadoGlobal);
+                    break;
+                default:
+                    res.status(400).json({ error: 'Invalid action for GET request.' });
+            }
+        } else {
+          res.setHeader('Allow', ['POST', 'GET']);
+          res.status(405).end(`Method ${req.method} Not Allowed`);
+        }
+    } catch (error) {
+        console.error('Internal server error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,107 @@
-
-import { useRouter } from 'next/router';
+import { useState} from 'react';
+import styles from '../styles/controle.module.css';
 
 
 const Index = () => {
-    const router = useRouter();
 
-    return (
-        <>
+        const [isDisabled, setIsDisabled] = useState(false);
+
+        const handleMotor = async () => {
+
+            const response = await fetch('/api/executar?action=direto', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({  func:'Motor' }),
+            });
+        
+            if (!response.ok) {
+                const data = await response.json();
+                alert(data.error || 'Erro ao registrar a ação');
+                // throw new Error('Erro ao registrar a ação');
+            }
+            //   const data = await response.json();
+            //   console.log('o valor da ação retornada é',data.action)
+            console.log('Estado registrado com sucesso');
+            setIsDisabled(true);
+            setTimeout(() => {
+                setIsDisabled(false);
+            }, 20000);
+
+      };
+
+        const handleNivel = async () => {
+
+            const response = await fetch('/api/executar?action=direto', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ func:'Nivel' }),
+            });
 
             
-                <div>
-                    <h1 >Conrado mama o Corsi</h1>
-                    <div >
+        
+            if (!response.ok) {
+                const data = await response.json();
+                alert(data.error || 'Erro ao registrar a ação');
+                // throw new Error('Erro ao registrar a ação');
+            }
+
+            const data = await response.json();
+            console.log(data)
+            console.log('Estado registrado com sucesso');
+            setIsDisabled(true);
+            setTimeout(() => {
+                setIsDisabled(false);
+            }, 20000);
+
+      };
+
+        const handleCarta = async () => {
+
+            const response = await fetch('/api/executar?action=direto', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ acao:'A60' }),
+            });
+
+            
+        
+            if (!response.ok) {
+                const data = await response.json();
+                alert(data.error || 'Erro ao registrar a ação');
+                // throw new Error('Erro ao registrar a ação');
+            }
+
+            const data = await response.json();
+            console.log(data)
+            console.log('Estado registrado com sucesso');
+            setIsDisabled(true);
+            setTimeout(() => {
+                setIsDisabled(false);
+            }, 20000);
+
+      };
 
 
+
+      return (
+
+
+        <div className={styles.pageContainer}>
+                    <h1 className={styles.title}>Comandos</h1>
+                    <div className={styles.buttonsContainer}>
+                        <button  className={styles.button} onClick={() => handleMotor()} disabled={isDisabled}>Motor</button>
+                        <button  className={styles.button} onClick={() => handleNivel()} disabled={isDisabled}>Nivel</button>
                     </div>
-                </div>
-              
-        </>
-    );
+        </div>
+      );
 };
 
 export default Index;
+
+
