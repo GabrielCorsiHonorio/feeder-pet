66a7d6243f481c1097f893a9e99b2f12f202ee41
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2025-09-19 00:28:50 +0200

    sem lastActionGlobal

--- pages/api/executar.js
+++ pages/api/executar.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
                               acaoGlobal = "A11";
                               console.log('Setando acaoglobal para ',{acaoGlobal});
                               res.status(201).json(acaoGlobal);
-                              lastActionGlobal = 'executando';
+                              lastActionGlobal = null;
                             } catch (error) {
                                 res.status(500).json({ error: "Failed to create action" });
                               } 
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
                             acaoGlobal = "A21";
                             console.log('Setando acaoglobal para',{acaoGlobal});
                               res.status(201).json(acaoGlobal);
-                              lastActionGlobal = 'executando';
+                              lastActionGlobal = null;
                             } catch (error) {
                                 res.status(500).json({ error: "Failed to create action" });
                               } 
