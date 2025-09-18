951a6ac74beade248ca83734405841afdf3e7016
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2025-09-18 17:14:08 +0200

    primeiro deploy

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+
+import { useRouter } from 'next/router';
+
+
+const Index = () => {
+    const router = useRouter();
+
+    return (
+        <>
+
+            
+                <div>
+                    <h1 >Gabriel Corsi Honório 2</h1>
+                    <div >
+
+
+                    </div>
+                </div>
+              
+        </>
+    );
+};
+
+export default Index;
